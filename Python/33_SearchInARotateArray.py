062a030b61aecd19c654b46fa81488968f026420
Collin Erickson <collinberickson@gmail.com>
2018-06-23 22:28:12 -0400

    Working on 33

--- Python/33_SearchInARotateArray.py
+++ Python/33_SearchInARotateArray.py
@@ -5,6 +5,8 @@ Created on Fri Jun 22 20:22:23 2018
 @author: cbe117
 """
 
+import math
+
 class Solution(object):
     def search(self, nums, target):
         """
@@ -20,9 +22,26 @@ class Solution(object):
                 return 0
             else:
                 return -1
-        if nums[lennums] < nums[0]:
-            pass
-        
-        return
+        # Find switch point with binary search
+        a = int(math.log(lennums, 2))
+        i = lennums / 2
+        while a > 0:
+            a /= 2
+            if nums[i] > nums[i+1]:
+                stop
+            
+        if nums[lennums / 2] < nums[0]:
+            if target >= nums[0] or target < nums[lennums / 2]:
+                return self.search(nums[0:(lennums/2)], target)
+            else:
+                return self.binary(nums[(lennums/2):lennums], target)
+        else:
+            if target >= nums[0] and target < nums[lennums / 2]:
+                return self.binary(nums[0:(lennums/2)], target)
+            else:
+                return self.search(nums[(lennums/2):lennums], target)
+        return "error"
+    def binary(self, nums, target):
+        pass
 sol = Solution()
 print (sol.search([4,5,6,7,0,1,2], 0))
