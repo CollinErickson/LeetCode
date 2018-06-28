4a4f3d0473a5cbb4ede1758bba4f081e54fee927
Collin Erickson <collinberickson@gmail.com>
2018-06-27 22:08:41 -0400

    Started 32

---
+++ Python/32_LongestValidParentheses.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 24 17:28:45 2018
+
+@author: cbe117
+"""
+
+class Solution(object):
+    def longestValidParentheses(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        
