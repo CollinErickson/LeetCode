959df70ea32001253f577af59a74e3258ae8465a
Collin Erickson <collinberickson@gmail.com>
2018-07-07 21:47:20 -0400

    Starting 34

---
+++ Python/34_SearchRange.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 07 21:13:23 2018
+
+@author: cbe117
+"""
+
+class Solution(object):
+    def searchRange(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        lennums = len(nums)
+        right = -1
+        left = lennums
+        return
+sol = Solution()
+print sol.searchRange(nums = [5,7,7,8,8,10], target = 8), [3,4]
