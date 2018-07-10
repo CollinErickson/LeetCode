5337cffb194f721448947087ba3f4080283751bb
Collin Erickson <collinberickson@gmail.com>
2018-07-09 22:06:07 -0400

    Working on 34, left side is close

--- Python/34_SearchRange.py
+++ Python/34_SearchRange.py
@@ -20,19 +20,35 @@ class Solution(object):
         if nums[0] == target:
             left = 0
         else:
-            diff = int(math.pow(math.ceil(math.log(lennums/2)), 2))
-            ii = diff # lennums / 2 - 1
-            while diff >=1:
-                diff //= 2
-                print diff, ii
-                if ii >= lennums:
-                    left = -1
-                elif nums[ii] <= target:
-                    ii += diff
+            leftlower = 0
+            leftupper = lennums - 1
+            while leftlower < leftupper:
+                if leftupper - leftlower == 1:
+                    break
+                tempind = leftlower + (leftupper - leftlower) // 2
+                if nums[tempind] < target:
+                    leftlower = tempind
                 else:
-                    ii -= diff
+                    leftupper = tempind
+            if nums[leftupper] == target:
+                left = leftupper
+            else:
+                left = -1
+            #diff = int(math.pow(math.ceil(math.log(lennums/2)), 2))
+            #ii = diff # lennums / 2 - 1
+            #while diff >=1:
+            #    diff //= 2
+            #    print diff, ii
+            #    if ii >= lennums:
+            #        left = -1
+            #    elif nums[ii] <= target:
+            #        ii += diff
+            #    else:
+            #        ii -= diff
+            #left = 333
         return [left, right]
     def binarySearch(self, nums, target):
         pass
 sol = Solution()
 print sol.searchRange(nums = [5,7,7,8,8,10], target = 8), [3,4]
+print sol.searchRange(nums = [5,7,7,8,8,10], target = 6), [-1,-1]
