527ffb7da7dce3013a53076df3f14cd035e591ac
Collin Erickson <collinberickson@gmail.com>
2018-07-15 23:04:23 -0400

    Starting 43

---
+++ Python/43_MultiplyStrings.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 15 23:02:24 2018
+
+@author: cbe117
+"""
+
+class Solution(object):
+    def multiply(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        return
+sol = Solution()
+print sol.multiply(num1 = "2", num2 = "3"), "6"
+print sol.multiply(num1 = "123", num2 = "456"), "56088"
