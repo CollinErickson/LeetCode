f52ed803cf497edc6c409118b5a364b296d99801
Collin Erickson <collinberickson@gmail.com>
2018-07-16 22:35:40 -0400

    Working on 43

--- Python/43_MultiplyStrings.py
+++ Python/43_MultiplyStrings.py
@@ -12,7 +12,13 @@ class Solution(object):
         :type num2: str
         :rtype: str
         """
-        return
+        total = ""
+        for i in reversed(num1):
+            
+            for j in reversed(num2):
+                ij = int(i) * int(j)
+                total = str(ij) + total
+        return total
 sol = Solution()
 print sol.multiply(num1 = "2", num2 = "3"), "6"
 print sol.multiply(num1 = "123", num2 = "456"), "56088"
