5e6f8155807a19b670cc451a363d56216055061a
Collin Erickson <collinberickson@gmail.com>
2018-07-17 22:03:19 -0400

    Working on 43, close

--- Python/43_MultiplyStrings.py
+++ Python/43_MultiplyStrings.py
@@ -13,12 +13,41 @@ class Solution(object):
         :rtype: str
         """
         total = ""
-        for i in reversed(num1):
-            
-            for j in reversed(num2):
+        len1 = len(num1)
+        len2 = len(num2)
+        ta = ["" for i in range(len1 + len2)]
+        for a, i in enumerate(reversed(num1)): 
+            for b, j in enumerate(reversed(num2)):
+                print a,i,b,j, ta
                 ij = int(i) * int(j)
                 total = str(ij) + total
-        return total
+                sij = str(ij)
+                #ta[len1+len2-a-b-1] = str(ij)
+                print len(sij), len1+len2-a-b-1
+                if len(sij) == 1:
+                    sij = " " + sij
+                print 'hee', ta[len1+len2-a-b-1]
+                if ta[len1+len2-a-b-1] == "":
+                    if ij >= 10:
+                        ta[len1+len2-a-b-1] = sij[1]
+                        ta[len1+len2-a-b-1-1] = sij[0]
+                    else:
+                        print 'here'
+                        ta[len1+len2-a-b-1] = sij
+                else: # Already a digit there
+                    tb = int(ta[len1+len2-a-b-1])
+                    tc = tb + ij
+                    if tc >= 10:
+                        ta[len1+len2-a-b-1] = str(tc - 10)
+                        ta[len1+len2-a-b-1-1] = str(tc // 10)
+                    else:
+                        ta[len1+len2-a-b-1] = str(tc)
+                #else:
+                #    # has length two
+                #    if 
+        print ta
+        return "".join(ta)
+        #return total
 sol = Solution()
-print sol.multiply(num1 = "2", num2 = "3"), "6"
+#print sol.multiply(num1 = "2", num2 = "3"), "6"
 print sol.multiply(num1 = "123", num2 = "456"), "56088"
