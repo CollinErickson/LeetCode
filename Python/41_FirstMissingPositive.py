3a54315cf1b8cc47af373cad21231def88c55dbb
Collin Erickson <collinberickson@gmail.com>
2018-07-19 22:43:17 -0400

    Starting 41

---
+++ Python/41_FirstMissingPositive.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 19 22:41:46 2018
+
+@author: cbe117
+"""
+
+class Solution(object):
+    def firstMissingPositive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+        return
+sol = Solution()
+print sol.firstMissingPositive([1,2,0]), 3
+print sol.firstMissingPositive([3,4,-1,1]), 1
+print sol.firstMissingPositive([7,8,9,11,12]), 1
