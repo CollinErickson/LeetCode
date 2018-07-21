4f2f6c23aaafda82998c4ccd9cabb6ae131cb314
Collin Erickson <collinberickson@gmail.com>
2018-07-21 16:06:44 -0400

    Almost got 41

--- Python/41_FirstMissingPositive.py
+++ Python/41_FirstMissingPositive.py
@@ -12,19 +12,36 @@ class Solution(object):
         :rtype: int
         """
         ln = len(nums)
+        if ln == 1:
+            return 2 if nums[0] == 1 else 1
         firstnotchecked = 0
         lastplace = ln - 1
         itocheck = 0
-        while itocheck < ln:
+        while itocheck <= lastplace:
+            #print itocheck, nums, firstnotchecked, lastplace
             x = nums[itocheck]
-            if x > lastplace:
+            if x > lastplace or x < 1 or (x!=itocheck+1 and nums[x-1] == x):
+                #print 'c1'
                 tx = nums[lastplace]
                 nums[lastplace] = x
                 nums[itocheck] = tx
+                lastplace -= 1
                 continue
+            elif x == itocheck + 1:
+                #print 'c2'
+                itocheck += 1
             else: # within range still
-        return
+                #print 'c3'
+                tx = nums[x - 1]
+                nums[x-1] = x
+                nums[itocheck] = tx
+        return itocheck + 1
 sol = Solution()
 print sol.firstMissingPositive([1,2,0]), 3
-print sol.firstMissingPositive([3,4,-1,1]), 1
+print sol.firstMissingPositive([3,4,-1,1]), 2
 print sol.firstMissingPositive([7,8,9,11,12]), 1
+print sol.firstMissingPositive([2]), 1
+print sol.firstMissingPositive([1]), 2
+print sol.firstMissingPositive([1,2,3]), 3
+print sol.firstMissingPositive([5,6,3,1,2,32]), 3
+print sol.firstMissingPositive([5,6,3,1,2,3,4]), 7
