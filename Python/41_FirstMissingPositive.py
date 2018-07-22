941b4e336e703b2dc85605612a2ba67ac6134eb7
Collin Erickson <collinberickson@gmail.com>
2018-07-22 16:52:15 -0400

    Finished 41

--- Python/41_FirstMissingPositive.py
+++ Python/41_FirstMissingPositive.py
@@ -14,19 +14,18 @@ class Solution(object):
         ln = len(nums)
         if ln == 1:
             return 2 if nums[0] == 1 else 1
-        firstnotchecked = 0
+        #firstnotchecked = 0
         lastplace = ln - 1
         itocheck = 0
         while itocheck <= lastplace:
-            #print itocheck, nums, firstnotchecked, lastplace
+            #print itocheck, nums, lastplace
             x = nums[itocheck]
-            if x > lastplace or x < 1 or (x!=itocheck+1 and nums[x-1] == x):
+            if x > lastplace+1 or x < 1 or (x!=itocheck+1 and nums[x-1] == x):
                 #print 'c1'
                 tx = nums[lastplace]
                 nums[lastplace] = x
                 nums[itocheck] = tx
                 lastplace -= 1
-                continue
             elif x == itocheck + 1:
                 #print 'c2'
                 itocheck += 1
@@ -42,6 +41,8 @@ print sol.firstMissingPositive([3,4,-1,1]), 2
 print sol.firstMissingPositive([7,8,9,11,12]), 1
 print sol.firstMissingPositive([2]), 1
 print sol.firstMissingPositive([1]), 2
-print sol.firstMissingPositive([1,2,3]), 3
-print sol.firstMissingPositive([5,6,3,1,2,32]), 3
+print sol.firstMissingPositive([1,2,3]), 4
+print sol.firstMissingPositive([5,6,3,1,2,32]), 4
 print sol.firstMissingPositive([5,6,3,1,2,3,4]), 7
+print sol.firstMissingPositive([5,6,3,1,2,3,7,4]), 8
+print sol.firstMissingPositive([5,6,3,1,2,7,4]), 8
