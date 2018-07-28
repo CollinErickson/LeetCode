24f72adfee25d394f7d90c1459030fd10e4687c2
Collin Erickson <collinberickson@gmail.com>
2018-07-27 22:23:00 -0400

    Stuck on 44

--- Python/44_WildcardMatching.py
+++ Python/44_WildcardMatching.py
@@ -14,15 +14,30 @@ class Solution(object):
         """
         ls = len(s)
         lp = len(p)
+        if ls ==0 and lp==0:
+            return True
+        if ls>0 and lp==0:
+            return False
+        if ls==0 and lp>0:
+            for j in range(lp):
+                if p[j] != "*":
+                    return False
+                return True
         m = [[False for j in range(lp)] for i in range(ls)]
-        m[0][0] = True
+        m[0][0] = (s[0] == p[0] or p[0]=='*' or p[0]=='?')
         for i in range(ls):
-            for j in range(ls):
+            for j in range(lp):
+                #print i,j
+                #for mr in m:print mr
                 if i > 0 and (m[i-1][j] and p[j]=="*"):
                     m[i][j] = True
-                if i>0 and m[i-1][j-1] and (p[j] == "?" or p[j] == "*" or s[i] == p[j]):
+                if i>0 and j>0 and m[i-1][j-1] and (p[j] == "?" or p[j] == "*" or s[i] == p[j]):
                     m[i][j] = True
-        return
+                #if j>0 and m[i][j-1] and (p[j-1]=="*" and (p[j]=="*" or p[j]==s[i])):
+                #    m[i][j] = True
+                if j>0 and (m[i][j-1] and p[j]=="*"):
+                    m[i][j] = True
+        return m[ls-1][lp-1]
     
 sol = Solution()
 print sol.isMatch(s = "aa", p = "a"), False
@@ -30,3 +45,17 @@ print sol.isMatch(s = "aa", p = "*"), True
 print sol.isMatch(s = "cb", p = "?a"), False
 print sol.isMatch(s = "abceb", p = "a*b"), True
 print sol.isMatch(s = "acdcb", p = "a*c?b"), False
+print sol.isMatch(s = "aa", p = "??"), True
+print sol.isMatch(s = "g", p = "?"), True
+print sol.isMatch(s = "aaffd", p = "*"), True
+print sol.isMatch(s = "sdfadfl", p = "***"), True
+print sol.isMatch(s = "g", p = ""), not True
+print sol.isMatch(s = "", p = "****"), True
+print sol.isMatch(s = "adceb", p = "*a*b"), True
+print sol.isMatch(s = "a", p = "*a"), True
+print sol.isMatch(s = "aab", p = "c*a*b"), False
+print sol.isMatch(s = "a", p = "a*"), True
+print sol.isMatch(s = "mississippi", p = "m??*ss*?i*pi"), False
+
+
+
