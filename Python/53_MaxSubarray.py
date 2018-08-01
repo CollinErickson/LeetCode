2b85ebb28dee39b5af5fa0516cc99a2eb67f1221
Collin Erickson <collinberickson@gmail.com>
2018-07-31 22:26:01 -0400

    Started 53

---
+++ Python/53_MaxSubarray.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 31 22:24:52 2018
+
+@author: cbe117
+"""
+
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+        return 1
+    
+sol = Solution()
+print(sol.maxSubArray([-2,1,-3,4,-1,2,1,-5,4]), 6)
