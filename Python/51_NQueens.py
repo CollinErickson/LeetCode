37c5a812d3a01044cd7d4749256a9abb9ffabedb
Collin Erickson <collinberickson@gmail.com>
2018-09-10 22:10:11 -0500

    Started 51

---
+++ Python/51_NQueens.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 10 22:04:53 2018
+
+@author: cbe117
+"""
+
+class Solution(object):
+    def solveNQueens(self, n):
+        """
+        :type n: int
+        :rtype: List[List[str]]
+        """
+        s = []
+        return
+sol = Solution()
+print(sol.solveNQueens(4))
