45af5be39041ad666ff05a24fdf043cb3747d0b0
Collin Erickson <collinberickson@gmail.com>
2018-09-12 21:48:45 -0500

    Should be solved, but submission giving different result from test

--- Python/51_NQueens.py
+++ Python/51_NQueens.py
@@ -6,17 +6,36 @@ Created on Mon Sep 10 22:04:53 2018
 """
 
 class Solution(object):
+    s = []
     def solveNQueens(self, n):
         """
         :type n: int
         :rtype: List[List[str]]
         """
-        s = []
-        return
-    def isValid(n, queens, checklastrow):
+        if n==1: return [["Q"]]
+        self.run([-1 for i in range(n)], index=0)
+        return self.s
+    def run(self, board, index):
+        #print(index, board)
+        # board is vector with rows of queens
+        # index is the current index to check, go left to right
+        # Check if board is filled out. If yes, it is solution, would have been
+        #  rejected earlier if not valid
+        if index == len(board):
+            tmp = ["."*i + "Q" + "."*(len(board)-i-1) for i in board]
+            #print(tmp, board)
+            self.s.append(tmp)
+            return
+        for i in range(len(board)):
+            board[index] = i
+            if self.isValid(n=index,board=board[:index], checklastrow=i):
+                self.run(board[:], index + 1)
+    def isValid(self, n, board, checklastrow):
         for i in range(n):
-            if abs(queens[i] - checklastrow) == i or queens[i]==checklastrow:
+            if abs(board[i] - checklastrow) == n-i or board[i]==checklastrow:
                 return False
         return True
 sol = Solution()
-print(sol.solveNQueens(4))
+#print(sol.solveNQueens(4))
+#print(sol.solveNQueens(5))
+print(sol.solveNQueens(7))
