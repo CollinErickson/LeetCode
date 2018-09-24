71e317932ed66110b8a0afe7d5ecd603f04696b4
Collin Erickson <collinberickson@gmail.com>
2018-09-23 21:32:14 -0500

    Working on 61

--- Python/61_RotateList.py
+++ Python/61_RotateList.py
@@ -10,6 +10,8 @@ class ListNode(object):
     def __init__(self, x):
         self.val = x
         self.next = None
+    def __repr__(self):
+        return str(self.val) + "->" + self.next.__repr__()
 
 class Solution(object):
     def rotateRight(self, head, k):
@@ -29,12 +31,35 @@ class Solution(object):
         for i in range(k):
             n = n.next
         newhead = n
+        print('newhead is', newhead)
         while n.next is not None:
             n = n.next
+        print('n is', n)
         n.next = head
-        n = head
         for i in range(k):
+            
             n = n.next
         n.next = None
         return newhead
 
+sol = Solution()
+n1 = ListNode(1)
+n2 = ListNode(2)
+n3 = ListNode(3)
+n4 = ListNode(4)
+n5 = ListNode(5)
+n1.next = n2
+n2.next = n3
+n3.next = n4
+n4.next = n5
+print(sol.rotateRight(n1,1), "\n5->1->2->3->4->NULL")
+n1 = ListNode(1)
+n2 = ListNode(2)
+n3 = ListNode(3)
+n4 = ListNode(4)
+n5 = ListNode(5)
+n1.next = n2
+n2.next = n3
+n3.next = n4
+n4.next = n5
+print(sol.rotateRight(n1,2), "\n4->5->1->2->3->NULL")
