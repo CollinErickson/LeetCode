8263340d111127a5cdf67dd3f39192c6dc066e40
Collin Erickson <collinberickson@gmail.com>
2018-09-24 22:24:48 -0500

    57

--- Python/57_InsertInterval.py
+++ Python/57_InsertInterval.py
@@ -20,10 +20,38 @@ class Solution(object):
         :type newInterval: Interval
         :rtype: List[Interval]
         """
-        
-        return intervals
+        l = len(intervals)
+        if l==0: return [newInterval]
+        for i in range(l):
+            if newInterval.start < intervals[i].start:
+                intervals = intervals[0:i] + [newInterval] + intervals[i:]
+                break
+        else:
+            intervals = intervals + [newInterval]
+        #print("sorted ints are", intervals)
+        l = len(intervals)
+        keep = [True for i in range(l)]
+        for i in range(l-1):
+            if intervals[i].end >= intervals[i+1].start:
+                intervals[i].end = max(intervals[i].end, intervals[i+1].end)
+                intervals[i+1] = intervals[i]
+                keep[i+1] = False
+        return [i for i,k in zip(intervals,keep) if k]
+        #return intervals
 
 sol = Solution()
-print(sol.insert([Interval(1,3),Interval(2,6),Interval(8,10),Interval(15,18)],
-                  Interval(6,7)), 
-                  [[1,6],[8,10],[15,18]])
+print(sol.insert([Interval(1,3),Interval(6,9)],
+                  Interval(2,5)), 
+                  [[1,5],[6,9]])
+print(sol.insert([Interval(1,2),Interval(3,5),Interval(6,7),Interval(8,10),Interval(12,16)],
+                  Interval(4,8)), 
+                  [[1,2],[3,10],[12,16]])
+print(sol.insert([Interval(1,3)],
+                  Interval(2,5)), 
+                  [[1,5]])
+print(sol.insert([],
+                  Interval(2,5)), 
+                  [[2,5]])
+#print(sol.insert([Interval(1,3),Interval(2,6),Interval(8,10),Interval(15,18)],
+#                  Interval(6,7)), 
+#                  [[1,6],[8,10],[15,18]])
