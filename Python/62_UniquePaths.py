8c3b2e68a03184ac081395dbad930c809070bffb
Collin Erickson <collinberickson@gmail.com>
2018-10-24 22:42:25 -0500

    Starting 62

---
+++ Python/62_UniquePaths.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 24 22:41:28 2018
+
+@author: cbe117
+"""
+
+class Solution:
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        
+        return n
+    
+sol = Solution()
+print(sol.uniquePaths(3,2), 3)
