ea90117ddbcff4a1b9bfdbf198686889aaafdf21
Collin Erickson <collinberickson@gmail.com>
2019-11-11 07:17:18 -0500

    68

--- Python/68_TextJustification.py
+++ Python/68_TextJustification.py
@@ -21,21 +21,39 @@ class Solution(object):
         #    if lencurline + len(words[i]) < maxWidth:
         #        pass
         for w in words:
-            if lencurline + len(w) < maxWidth:
+            #print(curline, lencurline, w)
+            if lencurline + len(w) <= maxWidth:
                 curline.append(w)
                 lencurline += len(w) + 1
-            else:
+            else: # start a new line
                 lines.append(curline)
                 curline = [w]
-                lencurline = len(w)
+                lencurline = len(w) + 1
         lines.append(curline)
         
         # Now format within each line
         lines2 = []
-        for line in lines:
-            nspacestotal = maxwidth - sum([len(w) for w in line])
-            if nspacestotal < len(line) - 1:
-                print("FAILURE")
+        for iline, line in enumerate(lines):
+            if iline < len(lines) - 1: # Not the last line
+                nspacestotal = maxWidth - sum([len(w) for w in line])
+                nbreaks = len(line) - 1
+                spacesperbreak = nspacestotal // max(1, nbreaks)
+                if nspacestotal < len(line) - 1:
+                    print("FAILURE")
+                l = ''
+                if len(line) > 1:
+                    for iw, w in enumerate(line):
+                        l += (w)
+                        if iw < len(line) - 1:
+                            l += ''.join([' ' for i in range(spacesperbreak + (nspacestotal - nbreaks*spacesperbreak>iw))])
+                else:
+                    print(line, lines)
+                    l = line[0] + ''.join([' ' for i in range(maxWidth - len(line[0]))])
+                lines2.append(l)
+            else: # Last line
+                l = ' '.join(line)
+                l += ''.join([' ' for i in range(maxWidth - len(l))])
+                lines2.append(l)
         return lines2
 
 sol = Solution()
@@ -46,3 +64,25 @@ print(
    "example  of text",
    "justification.  "
 ])
+    
+print(sol.fullJustify(["What","must","be","acknowledgment","shall","be"], 16))
+print([
+  "What   must   be",
+  "acknowledgment  ",
+  "shall be        "
+])
+    
+print(sol.fullJustify(["Science","is","what","we","understand","well","enough","to","explain",
+         "to","a","computer.","Art","is","everything","else","we","do"], 20))
+print([
+  "Science  is  what we",
+  "understand      well",
+  "enough to explain to",
+  "a  computer.  Art is",
+  "everything  else  we",
+  "do                  "
+])
+
+
+print(sol.fullJustify(["Listen","to","many,","speak","to","a","few."], 6))
+print(sol.fullJustify(["Science","is","what","we","understand","well","enough","to","explain","to","a","computer.","Art","is","everything","else","we","do"], 20))
