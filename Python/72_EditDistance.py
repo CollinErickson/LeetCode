dd236f11059eb4549443634f88b61d24f51e249c
Collin Erickson <collinberickson@gmail.com>
2019-11-29 23:15:45 -0600

    started 72

---
+++ Python/72_EditDistance.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def minDistance(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: int
+        """
+        
+        return True
+
+sol = Solution()
+print(sol.minDistance("horse","ros"), 3)
+print(sol.minDistance("intention","execution"), 4)
