645403a268ece3d0c2f9d5c36aa5468896b5a926
Collin Erickson <collinberickson@gmail.com>
2019-12-01 21:21:20 -0600

    working on 71

--- Python/71_SimplifyPath.py
+++ Python/71_SimplifyPath.py
@@ -5,7 +5,25 @@ class Solution(object):
         :rtype: str
         """
         
-        return 1
+        s = path.split("/")
+        i = 0
+        while True:
+            #print(s, len(s), i)
+            if s[i] == "." or s[i] == "":
+                s = s[0:i] + s[(i+1):len(s)]
+            elif s[i] == "..":
+                if i==0:
+                    s = s[(i+1):len(s)]
+                else:
+                    s = s[0:(i-1)] + s[(i+1):len(s)]
+                    i -= 1
+            else:
+                i += 1
+            if i >= len(s):
+                break
+        t = "/" + "/".join(s)
+        
+        return t
     
 
 sol = Solution()
