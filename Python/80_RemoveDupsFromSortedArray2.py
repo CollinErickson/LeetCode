c591f9a8c49661e2f056003fb3c37da439d5bd93
Collin Erickson <collinberickson@gmail.com>
2019-12-08 18:28:01 -0500

    started 80

---
+++ Python/80_RemoveDupsFromSortedArray2.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+sol = Solution()
+print(sol.removeDuplicates([1,1,1,2,2,3]), 5)
