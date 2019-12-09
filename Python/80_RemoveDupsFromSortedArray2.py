d833a9ea8f3dbef8b005747fe47ea16dae8d8a33
Collin Erickson <collinberickson@gmail.com>
2019-12-09 08:58:17 -0500

    working on 80

--- Python/80_RemoveDupsFromSortedArray2.py
+++ Python/80_RemoveDupsFromSortedArray2.py
@@ -4,6 +4,36 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        if not nums:
+            return 0
+        lastkeepind = 0
+        lastnum = None
+        lastnumcount = 0
+        keepcount = 0
+        for num in nums:
+            keep = True
+            if num == lastnum:
+                lastnumcount += 1
+                if lastnumcount > 2:
+                    keep = False
+            else:
+                lastnum = num
+                lastnumcount = 1
+            if keep:
+                nums[lastkeepind] = num
+                lastkeepind += 1
+                keepcount += 1
+                nums[lastkeepind] = num
+                pass
+            else:
+                pass
+            #print(num, lastnum, lastnumcount, keep)
+        #print(lastkeepind)
+        if lastkeepind < len(nums):
+            nums = nums[0:lastkeepind]
+        return nums #len(nums)
         
 sol = Solution()
-print(sol.removeDuplicates([1,1,1,2,2,3]), 5)
+a1 = [1,1,1,2,2,3]
+print(sol.removeDuplicates(a1), 5)
+print(a1)
