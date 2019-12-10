8798d239e37dc684da08e11ac400374891dd2be5
Collin Erickson <collinberickson@gmail.com>
2019-12-10 09:15:16 -0500

    started 81

---
+++ Python/81_SearchInRotatedSortedArray2.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: bool
+        """
+        
+        
+        return
+
+
+sol = Solution()
+print(sol.search(nums = [2,5,6,0,0,1,2], target = 0), True)
