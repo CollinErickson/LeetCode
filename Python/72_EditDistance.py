cec03a989ab61bc4623fc9e443e4948cbdf6f067
Collin Erickson <collinberickson@gmail.com>
2019-12-13 22:21:16 -0500

    72

--- Python/72_EditDistance.py
+++ Python/72_EditDistance.py
@@ -5,9 +5,25 @@ class Solution(object):
         :type word2: str
         :rtype: int
         """
+        n = len(word1)
+        m = len(word2)
+        d = [[0 for i in range(m+1)] for j in range(n+1)]
         
-        return True
+        for i in range(n+1):
+            for j in range(m+1):
+                if i == 0 and j==0:
+                    d[i][j] = 0
+                elif i == 0 or j==0:
+                    d[i][j] = max(i, j)
+                else:
+                    if word1[i-1] == word2[j-1]:
+                        d[i][j] = d[i-1][j-1]
+                    else:
+                        d[i][j] = 1 + min(d[i-1][j], d[i][j-1], d[i-1][j-1])
+        #print(d)
+        return d[n][m]
 
 sol = Solution()
 print(sol.minDistance("horse","ros"), 3)
-print(sol.minDistance("intention","execution"), 4)
+print(sol.minDistance("horse","horse"), 0)
+print(sol.minDistance("intention","execution"), 5)
