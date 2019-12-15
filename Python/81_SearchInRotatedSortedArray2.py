b13377820ae04cc9b9466b27d6b36e8222f32457
Collin Erickson <collinberickson@gmail.com>
2019-12-15 16:29:20 -0500

    working on 81

--- Python/81_SearchInRotatedSortedArray2.py
+++ Python/81_SearchInRotatedSortedArray2.py
@@ -36,17 +36,24 @@ class Solution(object):
             return "error 0"
         if lennums == 1:
             return 0
+        if lennums == 1:
+            return -1 if nums[0]<nums[1] else 1
         curindex = 0
         shiftright = lennums // 2
         while True:
             print("w:", curindex, shiftright)
             if nums[curindex + shiftright] >= nums[0]:
                 curindex += shiftright
-            if nums[curindex + shiftright - 1] >= nums[curindex + shiftright]:
+            elif nums[curindex+1] < nums[curindex]:
+                return curindex+1
+            if nums[curindex - 1] >= nums[curindex]:
+                print('r:', curindex, shiftright)
                 return curindex + shiftright
             shiftright = shiftright // 2
+            while curindex + shiftright > lennums:
+                shiftright = shiftright // 2
             if shiftright == 0:
-                return "failed"
+                return -1
         return "failed2"
         
     def binary(self, nums, target, offset=0):
@@ -66,8 +73,11 @@ class Solution(object):
 
 sol = Solution()
 #print(sol.search(nums = [2,5,6,0,0,1,2], target = 0), True)
+print(sol.binary_index(nums = [0,1]), -1)
 print(sol.binary_index(nums = [1,0]), 1)
+print(sol.binary_index(nums = [1,0, 1]), 1)
 print(sol.binary_index(nums = [2,5,6,0,0,1,2]), 3)
 print(sol.binary_index(nums = [2,5,6,6,9,0,0,1,2]), 5)
+print(sol.binary_index(nums = [2,5,6,6,9,10,0,0,1,2,3,3,4]), 6)
 
 
