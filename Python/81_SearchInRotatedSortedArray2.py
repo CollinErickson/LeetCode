b85d83e9ba9fa0a014ea82cf64b6c6fb0db5e186
Collin Erickson <collinberickson@gmail.com>
2019-12-17 07:55:27 -0500

    working on 81

--- Python/81_SearchInRotatedSortedArray2.py
+++ Python/81_SearchInRotatedSortedArray2.py
@@ -28,7 +28,7 @@ class Solution(object):
             else:
                 return self.binary(nums[(lennums/2):lennums], target, offset+lennums/2)
         else:
-            print(target, nums, lennums, lennums // 2)
+            #print(target, nums, lennums, lennums // 2)
             if target >= nums[0] and target < nums[lennums // 2]:
                 return self.binary(nums[0:(lennums//2)], target, offset)
             else:
@@ -47,13 +47,13 @@ class Solution(object):
         curindex = 0
         shiftright = lennums // 2
         while True:
-            print("w:", curindex, shiftright)
+            #print("w:", curindex, shiftright)
             if nums[curindex + shiftright] >= nums[0]:
                 curindex += shiftright
             elif nums[curindex+1] < nums[curindex]:
                 return curindex+1
             if nums[curindex - 1] >= nums[curindex]:
-                print('r:', curindex, shiftright)
+                #print('r:', curindex, shiftright)
                 return curindex + shiftright
             shiftright = shiftright // 2
             while curindex + shiftright > lennums:
@@ -72,9 +72,9 @@ class Solution(object):
             if target == nums[0]:
                 return 0 + offset
             return -1
-        if target < nums[lennums/2]:
-            return self.binary(nums[0:(lennums/2)], target, offset)
-        return self.binary(nums[(lennums/2):lennums], target, offset=offset + lennums/2)
+        if target < nums[lennums//2]:
+            return self.binary(nums[0:(lennums//2)], target, offset)
+        return self.binary(nums[(lennums//2):lennums], target, offset=offset + lennums//2)
 
 
 sol = Solution()
@@ -87,11 +87,11 @@ if False:
     print(sol.binary_index(nums = [2,5,6,6,9,0,0,1,2]), 5)
     print(sol.binary_index(nums = [2,5,6,6,9,10,0,0,1,2,3,3,4]), 6)
 if True:
-    print(sol.search(nums = [0,1], target=1), -1)
-    print(sol.search(nums = [1,0]), 1)
-    print(sol.search(nums = [1,0, 1]), 1)
-    print(sol.search(nums = [2,5,6,0,0,1,2]), 3)
-    print(sol.search(nums = [2,5,6,6,9,0,0,1,2]), 5)
-    print(sol.search(nums = [2,5,6,6,9,10,0,0,1,2,3,3,4]), 6)
+    print(sol.search(nums = [0,1], target=1), 1)
+    print(sol.search(nums = [1,0], target=1), 0)
+    print(sol.search(nums = [1,0, 1], target=1), 0)
+    print(sol.search(nums = [2,5,6,0,0,1,2], target=1), 5)
+    print(sol.search(nums = [2,5,6,6,9,0,0,1,2], target=1), 7)
+    print(sol.search(nums = [2,5,6,6,9,10,0,0,1,2,3,3,4], target=1), 8)
 
 
