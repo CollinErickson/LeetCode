3f47e4aeef06d7fe0265a2639e3382f2a3e58c54
Collin Erickson <collinberickson@gmail.com>
2019-12-27 10:10:18 -0600

    84 first sol was too slow

--- Python/84_LargestRectInHist.py
+++ Python/84_LargestRectInHist.py
@@ -11,8 +11,39 @@ class Solution(object):
         :type heights: List[int]
         :rtype: int
         """
+        if (len(heights) == 0):
+            return 0
+        maxarea = 0
+        st = []
+        for i in range(len(heights)):
+            h = heights[i]
+            if len(st) == 0:
+                st.append(heights[i])
+            else:
+                width = 0
+                print(st)
+                while (len(st) > 0) and st[-1] >= heights[i]:
+                    width += 1
+                    p = st.pop()
+                    area = width * p
+                    maxarea = max(maxarea, area)
+        return maxarea
+    def largestRectangleAreaSlow(self, heights):
+        """
+        :type heights: List[int]
+        :rtype: int
+        """
+        maxarea = 0
+        for l in range(len(heights)):
+            lowest = heights[l]
+            for r in range(l+1, len(heights) + 1):
+                lowest = min(lowest, heights[r-1])
+                area = lowest * (r - l)
+                maxarea = max(maxarea, area)
+        return maxarea
+                
         
-        return 1
 
 sol = Solution()
 print(sol.largestRectangleArea([2,1,5,6,2,3]), 10)
+print(sol.largestRectangleArea([i for i in range(20000)]), 20000)
