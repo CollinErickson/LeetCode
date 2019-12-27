971630cfd041f243452e47df39733947ff1549bb
Collin Erickson <collinberickson@gmail.com>
2019-12-26 20:42:37 -0500

    started 84

---
+++ Python/84_LargestRectInHist.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 25 21:17:25 2019
+
+@author: cbe117
+"""
+
+class Solution(object):
+    def largestRectangleArea(self, heights):
+        """
+        :type heights: List[int]
+        :rtype: int
+        """
+        
+        return 1
+
+sol = Solution()
+print(sol.largestRectangleArea([2,1,5,6,2,3]), 10)
