add835ccf56f607ec1ad5ddf019f3eff6cfcfd07
Collin Erickson <collinberickson@gmail.com>
2020-07-17 19:53:23 -0400

    Working on 85

--- Python/85_MaximalRectangle.py
+++ Python/85_MaximalRectangle.py
@@ -1,10 +1,18 @@
 class Solution(object):
   def maximalRectangle(self, matrix):
-  """
-        :type matrix: List[List[str]]
-        :rtype: int
-        """
-        return False
+    """
+    :type matrix: List[List[str]]
+    :rtype: int
+    """
+    for i in range(len(matrix)):
+      for j in range(len(matrix[0])):
+        1
+    return False
         
 sol = Solution()
-sol.maximalRectangle()
+print(sol.maximalRectangle([
+  ["1","0","1","0","0"],
+  ["1","0","1","1","1"],
+  ["1","1","1","1","1"],
+  ["1","0","0","1","0"]
+]), 6)
