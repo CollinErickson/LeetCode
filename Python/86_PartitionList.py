4e866ee6a2a5a1b0685c941d13667915144851b5
Collin Erickson <collinberickson@gmail.com>
2020-07-25 22:43:59 -0400

    Started 86

---
+++ Python/86_PartitionList.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution(object):
+    def partition(self, head, x):
+        """
+        :type head: ListNode
+        :type x: int
+        :rtype: ListNode
+        """
+        
+        return
+
+sol = Solution()
+
+l1 = ListNode(1)
+l2 = ListNode(2)
+l3 = ListNode(3)
+l4 = ListNode(4)
+l5 = ListNode(5)
+
+print(sol.partition(), 3)
