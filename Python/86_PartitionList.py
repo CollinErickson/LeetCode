7ddcbd961de076936fc2b5e747be44d47ff5c1cb
Collin Erickson <collinberickson@gmail.com>
2020-07-26 20:02:51 -0400

    Working on 86

--- Python/86_PartitionList.py
+++ Python/86_PartitionList.py
@@ -3,6 +3,12 @@ class ListNode(object):
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+    def __repr__(self):
+        s = str(self.val)
+        if self.next is None:
+            return s + "\n"
+        else:
+            return s + " " + self.next.__repr__()
 class Solution(object):
     def partition(self, head, x):
         """
@@ -11,7 +17,7 @@ class Solution(object):
         :rtype: ListNode
         """
         
-        return
+        return head
 
 sol = Solution()
 
@@ -20,5 +26,10 @@ l2 = ListNode(2)
 l3 = ListNode(3)
 l4 = ListNode(4)
 l5 = ListNode(5)
+l22 = ListNode(2)
 
-print(sol.partition(), 3)
+l1.next = l2
+
+print('p1', (l1))
+
+print("s1", sol.partition(l1, 3))
