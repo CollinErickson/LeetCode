711cb210fb613dcc8ea0e6aa94b5ed3f0840ad3e
Collin Erickson <collinberickson@gmail.com>
2020-07-27 22:21:15 -0400

    Started 87

---
+++ Python/87_ScrambleString.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def isScramble(self, s1, s2):
+        """
+        :type s1: str
+        :type s2: str
+        :rtype: bool
+        """
+        return
+        
+sol = Solution()
+
+print(sol.isScramble(), False)
