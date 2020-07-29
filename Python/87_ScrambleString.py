a506212d99b1dbeda247d07e4b2c348e97962476
Collin Erickson <collinberickson@gmail.com>
2020-07-28 22:13:53 -0400

    Working on 87

--- Python/87_ScrambleString.py
+++ Python/87_ScrambleString.py
@@ -5,8 +5,23 @@ class Solution(object):
         :type s2: str
         :rtype: bool
         """
-        return
+        # If equal, done
+        if s1 == s2:
+          return True
+        # Check that they have same character frequencies
+        a1 = [0 for i in range(26)]
+        a2 = [0 for i in range(26)]
+        for i in range(len(s1)):
+          a1[ord(s1[i])-97] += 1
+          a2[ord(s2[i])-97] += 1
+        for i in range(len(s1)):
+          if a1[i] != a2[i]:
+            return False
+        # Now check recursively, the hard part
+        
+        return False
         
 sol = Solution()
 
-print(sol.isScramble(), False)
+print(sol.isScramble("great", "rgeat"), True)
+print(sol.isScramble("abcde", "caebd"), False)
