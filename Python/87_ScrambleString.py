ec9663a72840c687b9016f18baf56f2bf1a989ce
Collin Erickson <collinberickson@gmail.com>
2020-07-29 20:29:53 -0400

    Working on 87, too slow

--- Python/87_ScrambleString.py
+++ Python/87_ScrambleString.py
@@ -6,6 +6,8 @@ class Solution(object):
         :rtype: bool
         """
         # If equal, done
+        if len(s1) != len(s2):
+          return False
         if s1 == s2:
           return True
         # Check that they have same character frequencies
@@ -17,7 +19,14 @@ class Solution(object):
         for i in range(len(s1)):
           if a1[i] != a2[i]:
             return False
+        del a1, a2
         # Now check recursively, the hard part
+        for i in range(1, len(s1)):
+          if self.isScramble(s1[0:i], s2[0:i]) and self.isScramble(s1[i:], s2[i:]):
+            return True
+          if self.isScramble(s1[0:i], s2[(len(s2)-i):]) and self.isScramble(s1[i:], s2[0:(len(s2)-i)]):
+            return True
+            
         
         return False
         
@@ -25,3 +34,12 @@ sol = Solution()
 
 print(sol.isScramble("great", "rgeat"), True)
 print(sol.isScramble("abcde", "caebd"), False)
+print(sol.isScramble("a", "a"), True)
+print(sol.isScramble("ba", "ab"), True)
+print(sol.isScramble("ab", "ab"), True)
+print(sol.isScramble("", ""), True)
+print(sol.isScramble("aa", "a"), False)
+print(sol.isScramble("ettztrgij", "irttgetzj"), False)
+print(sol.isScramble("phlvandlvyupcq", "paplyvvdhnulcq"), False)
+"phlvandlvyupcq"
+"paplyvvdhnulcq"
