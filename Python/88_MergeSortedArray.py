a73bce5e534f289f9f8d80383cb357c4cac624ec
Collin Erickson <collinberickson@gmail.com>
2020-07-31 20:24:41 -0400

    Working on 88

--- Python/88_MergeSortedArray.py
+++ Python/88_MergeSortedArray.py
@@ -7,6 +7,37 @@ class Solution(object):
         :type n: int
         :rtype: None Do not return anything, modify nums1 in-place instead.
         """
+        iplace = 0
+        isave = None
+        isavenext = m
+        j = 0
+        x1 = nums1[0]
+        x2 = nums2[0]
+        while True:
+          if x1 is None and x2 is None:
+            break
+          if x2 is None or x1 <= x2:
+            if isave is None:
+              # None are saved, just increase index
+              iplace += 1
+              x1 = nums[iplace]
+            else:
+              nums1[iplace] = x1
+              iplace += 1
+              
+          else: # nums2 is less
+            nums1[iplace] = nums2[j]
+            j += 1
+            if j < n:
+              # Move nums1 into save spot
+              if x1 is not None:
+                
+              nums1[iplace] = x2
+              x2 = nums2[j]
+            else:
+              x2 = None
+            
+          break
         
         return nums1
 
