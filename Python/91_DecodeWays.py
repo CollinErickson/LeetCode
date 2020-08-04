11bea735fe9aecc178bd8c8c96cc5078e8fca5a0
Collin Erickson <collinberickson@gmail.com>
2020-08-03 22:20:28 -0400

    Started 91

---
+++ Python/91_DecodeWays.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def numDecodings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        
+        return 1
+        
+
+sol = Solution()
+print(sol.numDecodings('12'), 2)
+print(sol.numDecodings('226'), 3)
