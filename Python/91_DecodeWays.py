9c5d5eafa4623ea6930e3980c84f5dc9bf4102c7
Collin Erickson <collinberickson@gmail.com>
2020-08-04 21:58:20 -0400

    Working on 91

--- Python/91_DecodeWays.py
+++ Python/91_DecodeWays.py
@@ -4,10 +4,19 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        
-        return 1
+        print('here', s)
+        if len(s) == 0:
+          return 0
+        isvalid1 = s[0] != "0"
+        if len(s) == 1:
+          return 0 #isvalid1
+        int2 = int(s[0:2])
+        isvalid2 = int(int2 > 0 and int2 < 26 and len(s)>2)
+        x = 1 + isvalid1 + isvalid2 + self.numDecodings(s[1:]) + self.numDecodings(s[2:])
+        return x
         
 
 sol = Solution()
 print(sol.numDecodings('12'), 2)
 print(sol.numDecodings('226'), 3)
+print(sol.numDecodings('30'), 0)
