904824c681492772064fafed675483c5fc516eb6
Collin Erickson <collinberickson@gmail.com>
2020-08-05 22:00:57 -0400

    Working on 91

--- Python/91_DecodeWays.py
+++ Python/91_DecodeWays.py
@@ -1,18 +1,28 @@
 class Solution(object):
-    def numDecodings(self, s):
+    def numDecodings(self, s, include=0):
         """
         :type s: str
         :rtype: int
         """
         print('here', s)
         if len(s) == 0:
-          return 0
+          return include
         isvalid1 = s[0] != "0"
         if len(s) == 1:
-          return 0 #isvalid1
+          return isvalid1
+        if s[0] == "0":
+          nd1 = 0
+        else:
+          nd1 = self.numDecodings(s[1:],1)
+        if nd1 == 0:
+          isvalid1 = 0
         int2 = int(s[0:2])
         isvalid2 = int(int2 > 0 and int2 < 26 and len(s)>2)
-        x = 1 + isvalid1 + isvalid2 + self.numDecodings(s[1:]) + self.numDecodings(s[2:])
+        nd2 = self.numDecodings(s[2:],1)
+        print('x', s, include, isvalid1, isvalid2, nd1, nd2)
+        include=0
+        x = include + isvalid1 + isvalid2 + nd1 + nd2
+        x = nd1 + nd2
         return x
         
 
