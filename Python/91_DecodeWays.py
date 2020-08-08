b7c3e9a80861f9d3392c33f0d24b2bd265385ac2
Collin Erickson <collinberickson@gmail.com>
2020-08-07 21:15:38 -0400

    Working on 91

--- Python/91_DecodeWays.py
+++ Python/91_DecodeWays.py
@@ -4,10 +4,10 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        print('here', s)
+        #print('here', s)
         if len(s) == 0:
           return include
-        isvalid1 = s[0] != "0"
+        isvalid1 = int(s[0] != "0")
         if len(s) == 1:
           return isvalid1
         if s[0] == "0":
@@ -17,9 +17,12 @@ class Solution(object):
         if nd1 == 0:
           isvalid1 = 0
         int2 = int(s[0:2])
-        isvalid2 = int(int2 > 0 and int2 < 26 and len(s)>2)
-        nd2 = self.numDecodings(s[2:],1)
-        print('x', s, include, isvalid1, isvalid2, nd1, nd2)
+        isvalid2 = int(int2 > 0 and int2 <= 26) # and len(s)>2)
+        if isvalid2>.5:
+          nd2 = self.numDecodings(s[2:],1)
+        else:
+          nd2 = 0
+        #print('x', s, include, isvalid1, isvalid2, nd1, nd2)
         include=0
         x = include + isvalid1 + isvalid2 + nd1 + nd2
         x = nd1 + nd2
@@ -30,3 +33,10 @@ sol = Solution()
 print(sol.numDecodings('12'), 2)
 print(sol.numDecodings('226'), 3)
 print(sol.numDecodings('30'), 0)
+print(sol.numDecodings('3'), 1)
+print(sol.numDecodings('0'), 0)
+print(sol.numDecodings('11'), 2)
+print(sol.numDecodings('10'), 1)
+print(sol.numDecodings('91'), 1)
+print(sol.numDecodings('111'), 3)
+print(sol.numDecodings('01'), 0)
