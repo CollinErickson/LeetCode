46f8241137947244f6c7064b84195a71d74dce9b
Collin Erickson <collinberickson@gmail.com>
2020-08-14 20:28:16 -0400

    Started 93

---
+++ Python/93_RestoreIPAddresses.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def restoreIpAddresses(self, s):
+        """
+        :type s: str
+        :rtype: List[str]
+        """
+        
+        l = []
+        
+        return l
+
+sol = Solution()
+
+print(sol.restoreIpAddresses("25525511135"), ["255.255.11.135","255.255.111.35"])
