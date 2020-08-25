87d311f9fa535ef1679af0c290935656d7870e3b
Collin Erickson <collinberickson@gmail.com>
2020-08-24 20:43:55 -0400

    87

--- Python/87_ScrambleString.py
+++ Python/87_ScrambleString.py
@@ -5,21 +5,15 @@ class Solution(object):
         :type s2: str
         :rtype: bool
         """
-        # If equal, done
-        if len(s1) != len(s2):
+        # Must have same length and same character frequency
+        if len(s1) != len(s2) or sorted(s1)!=sorted(s2):
           return False
+        # If equal, done
         if s1 == s2:
           return True
-        # Check that they have same character frequencies
-        a1 = [0 for i in range(26)]
-        a2 = [0 for i in range(26)]
-        for i in range(len(s1)):
-          a1[ord(s1[i])-97] += 1
-          a2[ord(s2[i])-97] += 1
-        for i in range(len(s1)):
-          if a1[i] != a2[i]:
-            return False
-        del a1, a2
+        # All with length 3 or less are scrambles, save time/depth
+        if len(s1) < 3.5:
+          return True
         # Now check recursively, the hard part
         for i in range(1, len(s1)):
           if self.isScramble(s1[0:i], s2[0:i]) and self.isScramble(s1[i:], s2[i:]):
@@ -27,7 +21,6 @@ class Solution(object):
           if self.isScramble(s1[0:i], s2[(len(s2)-i):]) and self.isScramble(s1[i:], s2[0:(len(s2)-i)]):
             return True
             
-        
         return False
         
 sol = Solution()
