7ad1631f0db824865a95cda7fd45e731e1226a7c
Collin Erickson <collinberickson@gmail.com>
2020-11-14 08:14:38 -0500

    124 with help

--- Python/124_BinTreeMaxPathSum.py
+++ Python/124_BinTreeMaxPathSum.py
@@ -13,6 +13,19 @@ class TreeNode(object):
         return s
 class Solution(object):
   def maxPathSum(self, root):
+    def rec(node):
+      nonlocal ans
+      if not node:
+        return 0
+      left = rec(node.left)
+      right = rec(node.right)
+      ans = max(ans, max(node.val, max(left, right)+node.val, left+right+node.val))
+      return max(node.val, left+node.val, right+node.val)
+    
+    ans = -1001
+    rec(root)
+    return ans
+  def maxPathSumBAD(self, root):
         """
         :type root: TreeNode
         :rtype: int
@@ -30,10 +43,26 @@ class Solution(object):
       R = (0,0)
     else:
       R = self.help(root.right)
-    # p1 is tree that is only below
-    p1 = max(L[1] + R[1] + root.val, L[0], R[0])
-    # p2 is one sided
-    p2 = root.val + max(L[1], R[1])
+    if root.left is None and root.right is None:
+      p1 = root.val
+      p2 = root.val
+    elif root.left is None and root.right is not None:
+      R = self.help(root.right)
+      p1 = max(R[1] + root.val, R[0], root.val)
+      p2 = root.val + max(R[1], 0)
+    elif root.left is not None and root.right is None:
+      L = self.help(root.left)
+      p1 = max(L[1] + root.val, L[0], root.val)
+      p2 = root.val + max(L[1], 0)
+    elif root.left is not None and root.right is not None:
+      L = self.help(root.left)
+      R = self.help(root.right)
+      # p1 is tree that is only below
+      p1 = max(L[1] + R[1] + root.val, L[1] + root.val, R[1] + root.val, L[0], R[0], root.val)
+      # p2 is one sided
+      p2 = root.val + max(L[1], R[1], 0)
+    else:
+      assert 1==0
     return (p1, p2)
         
 sol = Solution()
@@ -67,3 +96,45 @@ n1 = TreeNode(-3)
 
 print(sol.maxPathSum(n1), -3)
 
+
+
+n1 = TreeNode(-11)
+n2 = TreeNode(2)
+n3 = TreeNode(3)
+n1.left = n2
+n1.right = n3
+
+print(sol.maxPathSum(n1), 3)
+
+n1 = TreeNode(1)
+n2 = TreeNode(-2)
+n3 = TreeNode(-3)
+n1.left = n2
+n1.right = n3
+
+print(sol.maxPathSum(n1), 1)
+
+
+n1 = TreeNode(-1)
+n2 = TreeNode(-2)
+n3 = TreeNode(-3)
+n1.left = n2
+n1.right = n3
+
+print(sol.maxPathSum(n1), -1)
+
+
+
+n1 = TreeNode(-1)
+n3 = TreeNode(9)
+n6 = TreeNode(-6)
+n7 = TreeNode(3)
+n8 = TreeNode(-2)
+n1.right = n3
+n3.left = n6
+n3.right = n7
+n7.right = n8
+#print(n1)
+print(sol.maxPathSum(n1), 12)
+print(sol.maxPathSum(n3), 12)
+print(sol.maxPathSum(n7), 3)
