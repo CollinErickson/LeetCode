d9a67818270c846003a6973a2c855605bbd35552
Collin Erickson <collinberickson@gmail.com>
2021-01-24 16:33:32 -0500

    135 done

--- Python/135_Candy.py
+++ Python/135_Candy.py
@@ -1,27 +1,15 @@
 class Solution:
     def candy(self, ratings):
-        if len(ratings) <=1:
-          return len(ratings)
-        total = 1
-        prev = 1
-        slack = 0
-        for i in range(1, len(ratings)):
-          if ratings[i] > ratings[i-1]:
-            # check for slack, if yes add to it
-            curr = prev + 1
-            total += curr
-            prev = curr
-          elif ratings[i] == ratings[i-1] or prev > 1:
-            curr = 1
-            total += curr
-            prev = curr
-          else: # Boost all previous (i has lower rating but previous got 1)
-            # Use slack
-            curr = 1
-            total += curr + i
-            prev = curr
-        return total
-
+      if len(ratings) <= 1:
+        return len(ratings)
+      x = [1 for r in ratings]
+      for i in range(1, len(ratings)):
+        if ratings[i] > ratings[i-1]:
+          x[i] = x[i-1] + 1
+      for i in range(len(ratings)-1, 0, -1):
+        if ratings[i-1] > ratings[i]:
+          x[i-1] = max(x[i-1], x[i] + 1)
+      return sum(x)
 sol = Solution()
 print(sol.candy([]), 0)
 print(sol.candy([4]), 1)
