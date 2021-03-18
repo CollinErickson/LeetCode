be607bd67366b04b0c6f946d0e5ade9baca91333
Collin Erickson <collinberickson@gmail.com>
2021-03-17 22:21:20 -0400

    114

--- Python/114_FlattenBinaryTree.py
+++ Python/114_FlattenBinaryTree.py
@@ -16,27 +16,62 @@ class Solution(object):
         :type root: TreeNode
         :rtype: None Do not return anything, modify root in-place instead.
         """
-        if root is None:
+        #print('start flatten', root)
+        if root is None or (root.left is None and root.right is None):
             return
         if root.left is None:
+            self.flatten(root.right)
             return
         if root.right is None:
-            pass
+            root.right = root.left
+            root.left  = None
+            self.flatten(root.right)
+            return
+        newright = root.left
+        oldright = root.right
+        self.flatten(oldright)
+        self.flatten(newright)
+        root.left = None
+        root.right = None
+        newrighttail = newright
+        while newrighttail.right is not None:
+            newrighttail = newrighttail.right
+        #print('new/old', newright, newrighttail, oldright)
+        newrighttail.right = oldright
+        root.right = newright
         return
 
 sol = Solution()
 
-n1 = TreeNode(5)
-n2 = TreeNode(4)
-n3 = TreeNode(8)
-n4 = TreeNode(11)
-#n5 = TreeNode(5)
-n6 = TreeNode(13)
-n7 = TreeNode(4)
-n8 = TreeNode(7)
-n9 = TreeNode(2)
-n10 = TreeNode(1)
-n11 = TreeNode(5)
+
+n1 = TreeNode(1)
+n2 = TreeNode(2)
+n1.left = n2
+print("Before:", n1)
+sol.flatten(n1)
+print("After: ", n1)
+
+
+n1 = TreeNode(1)
+n2 = TreeNode(2)
+n3 = TreeNode(3)
+n1.left = n2
+n1.right = n3
+print("Before:", n1)
+sol.flatten(n1)
+print("After: ", n1)
+
+n1 = TreeNode(1)
+n2 = TreeNode(2)
+n3 = TreeNode(3)
+n4 = TreeNode(4)
+n5 = TreeNode(5)
+n6 = TreeNode(6)
+n7 = TreeNode(7)
+n8 = TreeNode(8)
+n9 = TreeNode(9)
+n10 = TreeNode(10)
+n11 = TreeNode(11)
 
 
 n1.left = n2
