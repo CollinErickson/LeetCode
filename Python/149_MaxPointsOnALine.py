a24e61aa5a71d271efafa8e0d38bdc931cf6f5a4
Collin Erickson <collinberickson@gmail.com>
2021-09-26 21:30:51 -0400

    Started 149

---
+++ Python/149_MaxPointsOnALine.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxPoints(self, points):
+        return
+
+
+sol = Solution()
+
+print(sol.maxPoints([[1,1],[2,2],[3,3]]), 3)
+print(sol.maxPoints([[1,1],[3,2],[5,3],[4,1],[2,3],[1,4]]), 4)
