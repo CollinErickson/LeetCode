b34cc1e88365dc2c717c51f8496a1b2f014809f9
Collin Erickson <collinberickson@gmail.com>
2021-10-15 22:03:37 -0400

    153

---
+++ Python/153_MinRotatedArray.py
@@ -0,0 +1,25 @@
+class Solution:
+  def findMin(self, nums):
+    l = len(nums)
+    if l <= 1:
+      return nums[0]
+    if l <= 5:
+      return min(nums)
+    if nums[0] < nums[l-1]:
+      return nums[0]
+    # Find where array is flipped
+    m = l//2
+    x = nums[m]
+    #print('m', m, x)
+    if x > nums[0]:
+      return self.findMin(nums[m:])
+    return self.findMin(nums[0:(m+1)])
+
+sol = Solution()
+print(sol.findMin([3,4,5,1,2]), 1)
+print(sol.findMin([4,5,6,7,0,1,2]), 0)
+print(sol.findMin([11,13,15,17]), 11)
+print(sol.findMin([11]), 11)
+print(sol.findMin([11,13]), 11)
+print(sol.findMin([13, 11]), 11)
+print(sol.findMin([4,5,6,1,2,3]), 1)
