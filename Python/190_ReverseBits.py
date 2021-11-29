dece6b90dcab013199445922d129327ff4b0d68c
Collin Erickson <collinberickson@gmail.com>
2021-11-29 10:43:39 -0600

    190

--- Python/190_ReverseBits.py
+++ Python/190_ReverseBits.py
@@ -1,13 +1,22 @@
 class Solution:
   def reverseBits(self, n):
-    print(n)
+    # return 1
+    # print(n)
     n2 = bin(n)[2:]
-    print(n2)
+    # print(n2, len(n2))
+    n3 = "".join(['0' for i in range(32-len(n2))]) + str(n2)
+    # print(n3, len(n3))
+    n4 = n3[::-1]
+    # print(n4, len(n4))
+    n5 = int(n4, 2)
+    # print(n5)
+    return n5
     # int(str(n)[::-1], 2)
 
 sol = Solution()
-print(sol.reverseBits(00000010100101000001111010011100))#, 964176192)
-# print(sol.reverseBits(11111111111111111111111111111101), 3221225471)
+# print(sol.reverseBits(0000101), 123444)
+print(sol.reverseBits(43261596), 964176192)
+print(sol.reverseBits(4294967293), 3221225471)
 # print(sol.reverseBits(), )
 # print(sol.reverseBits(), )
 # print(sol.reverseBits(), )
